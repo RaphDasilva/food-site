38d3c24510a064048e64252a1f9efbaf8837cbf5
Okolo Raphael <okolonnaemeka2017@gmail.com>
2023-11-06 15:00:17 +0100

    Add data file

---
+++ src/componente/utilts/data.js
@@ -0,0 +1,22 @@
+export const links = [
+  {
+    id: 1,
+    url: '#home',
+    text: 'home',
+  },
+  {
+    id: 2,
+    url: '#about',
+    text: 'about',
+  },
+  {
+    id: 3,
+    url: '#product',
+    text: 'product',
+  },
+  {
+    id: 4,
+    url: '#contact',
+    text: 'contact',
+  },
+];
