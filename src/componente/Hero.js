d016e9af9e2f1faad7a91b436be0fa1e3413a050
Okolo Raphael <okolonnaemeka2017@gmail.com>
2023-11-06 14:59:45 +0100

    Add hero section

---
+++ src/componente/Hero.js
@@ -0,0 +1,20 @@
+const Hero = () => {
+  return (
+    <section className='hero__content' id='home'>
+      <div className='left-hero-section'></div>
+      <div className='right-hero-section'>
+        <div className='hero-text'>
+          <h1>Tessy Food</h1>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam
+            quidem, cumque, voluptates, voluptatem quas quod quia voluptatum
+            doloremque quos dolorum quae? Quisquam, voluptatem. Quisquam
+            voluptatem, quae voluptates quos dolorum quia.
+          </p>
+          <button>Order Now</button>
+        </div>
+      </div>
+    </section>
+  );
+};
+export default Hero;
